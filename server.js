0a35ec1181e5e0316afe43a89f1d4f215c744fa8
aljer80 <jernberg.alexandra@gmail.com>
2022-12-29 10:51:47 +0100

    ..

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;              //variabel för att lagra porten. Behöver man inte göra, men är vanligt. 
-const fs = require("fs");
+//const port = 3000;              //variabel för att lagra porten. Behöver man inte göra, men är vanligt. 
+//const fs = require("fs");
 
 //Definierar en endpoint. Och app.get bestämmer att det är request-metoden get endpointen ska lyssna efter. 
-app.get("/api/players/", (req, res) => {         
+app.get("/api/players", (req, res) => {         
     res.status(200).send("Här är alla spelare " + req.params);
 });
 
